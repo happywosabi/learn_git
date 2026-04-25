c932da1806114d3d2bab23c6ddad6dcd19ba5831
NN2007 <1213567677@qq.com>
2026-04-26 01:02:45 +0800

    提交作业

---
+++ solution.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+signed main(){
+    int a,b;
+    cout<<"请输入a:";
+    cin>>a;
+    cout<<"请输入b:";
+    cin>>b;
+    cout<<"a+b="<<a+b<<endl;
+    return 0;
+}
